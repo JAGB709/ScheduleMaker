f86b51556aea8b319edc46bbba2f9c8d240c05f5
Jose <jagb709@gmail.com>
2025-07-13 15:42:52 +0000

    arreglos

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.schedulesnap.app',
   appName: 'mi-aplicacion-next',
   webDir: 'out'
 };
