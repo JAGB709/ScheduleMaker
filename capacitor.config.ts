17204338869a5dac2191941d2e1c2b9bd9fadabb
Jose <jagb709@gmail.com>
2025-07-21 16:35:52 +0000

    eliminar todo lo relacionado con app movil solo dejalo en aplicacion web

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.schedulesnap.app',
-  appName: 'mi-aplicacion-next',
-  webDir: 'out'
-};
-
-export default config;
